fa73a2af78e4c4811d9574176a287974844f59e8
smile <2384064712@qq.com>
2025-03-10 23:26:47 +0800

    留了个评论接口

--- src/main/java/com/blog/myblog/controller/home/CommentController.java
+++ src/main/java/com/blog/myblog/controller/home/CommentController.java
@@ -8,13 +8,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Slf4j
 @RestController
-@Tag(name= "评论相关接口")
+@RequestMapping("/home/comment")
 public class CommentController {
 }
 
