ca8945ffe95678b2f3fbba9802a455cb7d196df4
smile <2384064712@qq.com>
2025-03-10 23:22:08 +0800

    留了个评论接口

---
+++ src/main/java/com/blog/myblog/controller/home/CommentController.java
@@ -0,0 +1,20 @@
+package com.blog.myblog.controller.home;
+
+import com.blog.myblog.entity.Comment;
+import com.blog.myblog.result.Result;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@Tag(name= "评论相关接口")
+public class CommentController {
+}
+
